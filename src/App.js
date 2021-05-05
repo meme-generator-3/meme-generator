90ac69f64c6738cbb6e86e7efaf127abc8159f2a
Camila Cortes <you@example.com>
2021-05-04 19:49:33 -0600

    deleted render memes component and display memes component since Meme.js is doing the job

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
- RefreshMeme-component
+
 import React, {} from 'react'
 import Meme from './components/Meme'
 
-import React from 'react';
-import RenderMemes from './components/RenderMemes'
- main
 
 
 const App = () => {
   return (
     <div>
-     RefreshMeme-component
+
       <Meme />
-      <RenderMemes/>
+  
     </div>
   )
 }
